08e8a6162a33a50a610c2a500e5322891905cdfe
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-25 17:35:44 +0400

    feat: implement Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
-    <h1 className='font-bold underline'>Hello I'm here!</h1>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/favourites' element={<FavouritesPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
