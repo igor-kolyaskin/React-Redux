7845853ee035b5d12b6cd548c8cb359afa816050
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-25 18:28:41 +0400

    feat: impleent github search

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSearchUsersQuery } from '../store/github/github.api'
 
 export function HomePage() {
+    const {isLoading, isError, data} = useSearchUsersQuery('igor')
     return (
         <div>Home</div>
     )
