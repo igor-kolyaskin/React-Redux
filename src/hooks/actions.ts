f5f1814e7ccbe88bc5309de0781639c8a6dc582f
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-26 12:50:23 +0400

    feat: implement slice for redux-toolkit

---
+++ src/hooks/actions.ts
@@ -0,0 +1,10 @@
+import { useDispatch } from "react-redux"
+import { bindActionCreators } from "redux"
+import { githubActions } from "../store/github/gitbub.slice"
+
+const actions = { ...githubActions }
+
+export const useActions = () => {
+    const dispatch = useDispatch()
+    return bindActionCreators(actions, dispatch)
+}
