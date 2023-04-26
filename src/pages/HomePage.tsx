3825527ce6bdbe018acd18705a46f8e33822cecb
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-26 08:00:34 +0400

    feat: implement input in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchUsersQuery } from '../store/github/github.api'
 
 export function HomePage() {
-    const {isLoading, isError, data} = useSearchUsersQuery('igor')
+    const [search, setSearch] = useState('')
+    const { isLoading, isError, data } = useSearchUsersQuery('igor')
+
+    useEffect(() => {
+        console.log(search)
+    }, [search])
+
     return (
-        <div>Home</div>
+        <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+            {isError && <p className='text-center text-red-600'>Something went wrong...</p>}
+
+            <div className='relative w-[560px]'>
+                <input
+                    type="text"
+                    className='border py-2 px-4 w-full h-[42px] mb-2'
+                    placeholder='Search for GitHub username...'
+                    value={search}
+                    onChange={event => setSearch(event.target.value)}
+                />
+                <div className='absolute top-[42px] left-0 right-0 bg-white max-h-[200px] shadow-md'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, alias?
+                </div>
+            </div>
+        </div>
     )
 }
